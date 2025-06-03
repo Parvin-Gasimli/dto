b56bd6bdc03c0d71c2955a9ddb2559c147dab46f
Parvin-Gasimli <parvinzga@code.edu.az>
2025-06-03 08:04:10 +0400

    dresses:page is creatimng

--- dto/server.js
+++ dto/server.js
@@ -1,40 +1,12 @@
-const express = require('express')
-const fs = require('fs')
-const app = express()
-
-app.use(express.json())
-
-app.get('/api', (req, res) => {
-  const data = JSON.parse(fs.readFileSync('db.json', 'utf-8'))
-  res.json(data)
-})
-
-app.get('/api/users', (req, res) => {
-  const data = JSON.parse(fs.readFileSync('db.json', 'utf-8'))
-  res.json(data.users)
-})
-app.get('/api/cars', (req, res) => {
-  const data = JSON.parse(fs.readFileSync('db.json', 'utf-8'))
-  res.json(data.cars)
-}
-)
-app.get('/api/rentals', (req, res) => {
-  const data = JSON.parse(fs.readFileSync('db.json', 'utf-8'))
-  res.json(data.rentals)
-}
-)
-app.get('/api/reviews', (req, res) => {
-  const data = JSON.parse(fs.readFileSync('db.json', 'utf-8'))
-  res.json(data.reviews)
-}
-)
-app.get('/api/contacts', (req, res) => {
-  const data = JSON.parse(fs.readFileSync('db.json', 'utf-8'))
-  res.json(data.contacts)
-}
-)
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
 
-app.listen(3000, () => {
-  console.log('Custom JSON server is running on http://localhost:3000/api')
+server.use(middlewares)
+server.use(router)
+
+server.listen(3000, () => {
+  console.log('JSON Server is running')
 })
